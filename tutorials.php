e3a6bb2613677f0bfe80c742854319e7f0d14798
Jeremy DeHaan <dehaan.jeremiah@gmail.com>
2018-10-03 20:58:30 -0700

    first pass at main tutorial page

--- tutorials.php
+++ tutorials.php
@@ -10,11 +10,51 @@
         <?php include 'header.php'?>
         <div class="inner" class="content">
             <h3>Site under construction!</h3>
-            <h2>Tutorials</a></h2>
-            <p>Temporary page for DSFML tutorials.</p>
+            <h1>Tutorials</h1>
+            <p>This series of tutorials for DSFML are based upon Laurent's tutorials for <a class="link" href="http://www.sfml-dev.org/tutorials/2.4/">SFML</a>. All content seen here should be credited to Laurent's original work. Changes have been made only where necessary to work with D and DSFML, instead of C++ and SFML.</p>
+            <h2 id="getting-started">Getting Started</h1>
+            <ul>
+                <li><a class="link" href="tutorials/buildingfromsource.php">Building From Source</a></li>
+                <li><a class="link" href="tutorials/firstprogram.php">Building Your First DSFML Program</a></li>
+            </ul>
+            <h2 id="system-module">System Module</h2>
+            <ul>
+                <li><a class="link" href="tutorials/time.php">Handling Time</a></li>
+                <li><a class="link" href="tutorials/streams.php">User Data Streams</a></li>
+            </ul>
+            <h2 id="window-module">Window Module</h2>
+            <ul>
+                <li><a class="link" href="tutorials/window.php">Opening and Managing a DSFML Window</a></li>
+                <li><a class="link" href="tutorials/events.php">Events Explained</a></li>
+                <li><a class="link" href="tutorials/inputs.php">Keyboard, Mouse, and Joysticks</a></li>
+            </ul>
+            <h2 id="graphics-module">Graphics Module</h2>
+            <ul>
+                <li><a class="link" href="tutorials/graphics-draw.php">Drawing 2D Stuff</a></li>
+                <li><a class="link" href="tutorials/sprites.php">Sprites and Textures</a></li>
+                <li><a class="link" href="tutorials/text.php">Text and Fonts</a></li>
+                <li><a class="link" href="tutorials/shapes.php">Shapes</a></li>
+                <li><a class="link" href="tutorials/vertex-arrays.php">Designing Your own Entities with Vertex Arrays</a></li>
+                <li><a class="link" href="tutorials/transforms.php">Position, Rotation, Scale: Transforming Entities</a></li>
+                <li><a class="link" href="tutorials/shaders.php">Adding Special Effects with Shaders</a></li>
+                <li><a class="link" href="tutorials/cameraviews.php">Controlling the 2D Camera with Views</a></li>
+            </ul>
+            <h2 id="audio-module">Audio Module</h2>
+            <ul>
+                <li><a class="link" href="tutorials/soundsandmusic.php">Playing Sounds and Music</a></li>
+                <li><a class="link" href="tutorials/recording.php">Recording Audio</a></li>
+                <li><a class="link" href="tutorials/customaudiostream.php">Custom Audio Stream</a></li>
+                <li><a class="link" href="tutorials/spatialization.php">Spatialization: Sounds in 3D</a></li>
+            </ul>
+            <h2 id="networking-module">Networking Module</h2>
+            <ul>
+                <li><a class="link" href="tutorials/sockets.php">Communicating with Sockets</a></li>
+                <li><a class="link" href="tutorials/packets.php">Using and Extending Packets</a></li>
+                <li><a class="link" href="tutorials/http.php">Web Requests with HTTP</a></li>
+                <li><a class="link" href="tutorials/ftp.php">File Transfers with FTP</a></li>
+            </ul>
         </div>
         <?php include 'footer.php'?>
     </div>
 </body>
 </html>
-
